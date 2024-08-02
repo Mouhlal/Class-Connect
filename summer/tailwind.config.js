1c3769a24b047acc91936436471bd89ca88b4348
Mouhlal <akramemouhlal@gmail.com>
2024-08-02 21:02:02 +0100

    Configuration de tailwind Css et vite js

---
+++ summer/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#3490dc',
+          secondary: '#ffed4a',
+          danger: '#e3342f',
+        },
+      },
+    },
+    plugins: [],
+  }
